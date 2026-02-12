a36302d4ee3640c2f8b22074e1ed2843f0706285
24DP3NLaci <A240675NL@rvt.lv>
2026-02-12 13:14:13 +0200

    Teacher, Person, Stundent,puspabeigti

---
+++ src/main/java/rvt/Student.java
@@ -0,0 +1,10 @@
+package rvt;
+
+public class Student extends Person {
+    private int credits;
+    public Student(String name, String address) {
+        super(name, address); // Person(name, address)
+
+    }
+    
+}
